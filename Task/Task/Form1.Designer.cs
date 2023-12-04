1bd34059ffda91a57c607e08a2abbfa32a4cb67b
Kamui34 <daviddicere@gmail.com>
2023-12-04 21:41:27 +0300

    Add files via upload

---
+++ Task/Task/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            label2 = new System.Windows.Forms.Label();
+            listBox1 = new System.Windows.Forms.ListBox();
+            label3 = new System.Windows.Forms.Label();
+            textBox2 = new System.Windows.Forms.TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(12, 27);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(526, 402);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(379, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Вставьте текст для поиска в нем e-mail адресов в область ниже:";
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(544, 27);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(228, 25);
+            button1.TabIndex = 2;
+            button1.Text = "Поиск";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(544, 58);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(228, 25);
+            button2.TabIndex = 3;
+            button2.Text = "Вывод результатов в файл";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(544, 136);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(201, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Предварительный список e-mail:";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new System.Drawing.Point(544, 154);
+            listBox1.Name = "listBox1";
+            listBox1.ScrollAlwaysVisible = true;
+            listBox1.Size = new System.Drawing.Size(228, 274);
+            listBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(544, 86);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(217, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Количество e-mail адресов в тексте:";
+            // 
+            // textBox2
+            // 
+            textBox2.Enabled = false;
+            textBox2.Location = new System.Drawing.Point(544, 104);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(228, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(784, 444);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(listBox1);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Приложение для секретаря-референта";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ Task/Task/Form1.cs
@@ -0,0 +1,85 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Task
+{
+    public partial class Form1 : Form
+    {
+        Task obj;
+        public Form1()
+        {
+            InitializeComponent();
+            obj = new Task();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text_all = textBox1.Text;
+            MatchCollection matches;
+
+            matches = obj.Module1(text_all);
+
+            textBox2.Text = obj.Module2(matches);
+            foreach (Match match in matches)
+            {
+                listBox1.Items.Add(match.Value);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            obj.Module3(listBox1.Items);
+        }
+    }
+
+    public class Task
+    {
+        public MatchCollection Module1(string text_all)
+        {
+            const string MatchEmailPattern =
+              @"(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+              + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+              + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+              + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})";
+
+            Regex rx = new Regex(
+              MatchEmailPattern,
+              RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            return rx.Matches(text_all);
+        }
+
+        public string Module2(MatchCollection matches)
+        {
+            return matches.Count.ToString();
+        }
+
+        public void Module3(ListBox.ObjectCollection list)
+        {
+            var workbook = new XLWorkbook();
+            workbook.AddWorksheet("sheetName");
+            var ws = workbook.Worksheet("sheetName");
+
+            ws.Column("A").Width = 50;
+
+            int row = 1;
+            foreach (object item in list)
+            {
+                ws.Cell("A" + row.ToString()).Value = item.ToString();
+                row++;
+            }
+
+            workbook.SaveAs("Список email.xlsx");
+        }
+    }
+}
